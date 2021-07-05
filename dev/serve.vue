90c45330e3cf5e660176780807026dfeda27320e
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-07-05 17:30:40 +0200

    Vue-sfc-rollup init
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { CalendarVueSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  CalendarVueSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <calendar-vue-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as CalendarVueSample } from './calendar-vue-sample.vue';
